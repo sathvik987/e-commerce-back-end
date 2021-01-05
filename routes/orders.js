c9cce2c130fc8886a2e3a389223e88b6aaeeefe5
Sathvik Patil <sathvikpatil67@gmail.com>
2021-01-05 20:09:47 +0530

    added stripe payment

--- routes/orders.js
+++ routes/orders.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const orders = require('../models/order_model')
+const Stripe = require('stripe')
+
+const stripe = new Stripe('sk_test_4YGOGclLFuIAIvc2obGqdj0Y00KKTEwBSX')
 
 
 router.post('/neworder', (req, res) => {
@@ -19,7 +22,20 @@ router.post('/neworder', (req, res) => {
             res.json("error" + err)
         })
 
+})
 
+router.post('/payment', async (req, res) => {
+    const { amount, email } = req.body
+
+    try {
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount: amount * 100,
+            currency: 'inr'
+        })
+        res.status(200).send(paymentIntent.client_secret)
+    } catch (err) {
+        res.status(500).send(err)
+    }
 
 })
 
