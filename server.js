b71a53b0adda25d0a07a22e126db5fbb4232c740
Sathvik patil <sathvikpatil67@gmail.com>
2023-07-06 22:39:03 +0530

    added dotenv

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors');
+require('dotenv').config()
 const users = require('./routes/users')
 const products = require('./routes/products')
 const orders = require('./routes/orders')
